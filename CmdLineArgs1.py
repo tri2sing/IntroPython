19ea49a10c5020fbb24e98a4d53571257a2f8783
tri2sing <tri2sing@yahoo.com>
2014-04-08 15:23:02 -0700

    Modified some files

--- CmdLineArgs1.py
+++ CmdLineArgs1.py
@@ -2,5 +2,14 @@ import sys
 
 if __name__ == '__main__':
     for i in range(len(sys.argv)):
+        #if i != 0: n = int(sys.argv[i])
         print('Arg %d is %s of type %s' % (i, sys.argv[i], type(sys.argv[i])))  # C-like
-        print(u'Arg {0:d} is {1:s} of type {2:s}'.format(i, sys.argv[i], type(sys.argv[i])))
+        #print(u'Arg {0:d} is {1:s} of type {2:s}'.format(i, sys.argv[i], type(sys.argv[i])))
+
+    # Change the type of the parameter
+    sys.argv[1] = int(sys.argv[1])
+    sys.argv[2] = float(sys.argv[2])
+
+    for i in range(len(sys.argv)):
+        print('Arg %d is %s of type %s' % (i, sys.argv[i], type(sys.argv[i])))  # C-like
+
--- CmdLineArgs2.py
+++ CmdLineArgs2.py
@@ -1,9 +1,10 @@
 import argparse
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument ('-c', '--count', type=int, default=1, help='the number of ice cream orders')
     parser.add_argument ('-f', '--flavor', default='vanilla', help='name of the flavor to use for the order')
     parser.add_argument ('-s', '--size', default='medium', help='default size of each order')
     args = parser.parse_args()
+    print(type(args))
     print("You ordered %d count of %s ice cream in %s size" % (args.count, args.flavor, args.size))
