d0f459d4140877c7e6f060ea0e1c208f47f81ce9
tri2sing <tri2sing@yahoo.com>
2014-04-08 10:14:19 -0700

    Modified HelloWorld

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,3 +1,8 @@
 if __name__ == '__main__':
-    name = input('Please enter your name: ')
+    name = input("Please enter your name: ")
     print('Hello World from ' + name)
+    n = int(input("Please enter a number: "))
+    print(n)
+    print(type(n))
+    print("Hello " * n + name)
+    print(("Hello " + name + ' ' ) * 3)
