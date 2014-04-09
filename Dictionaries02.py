cf61ded9935dbfeab9dcfcdfd4d4cc137d7c2fe3
tri2sing <tri2sing@yahoo.com>
2014-04-09 10:36:52 -0700

    Modified Dictionaries02 in class

--- Dictionaries02.py
+++ Dictionaries02.py
@@ -2,9 +2,24 @@
 if __name__ == '__main__':
     # Dict as a sparse list
     print('\n')
-    L1 = {0: 'a', 5: 'r', 10: 'z'}
+    L1 = {0: 'are', 5: 'you', 99: 'home'}
     print(L1)
+    print(L1[0])
+    print(L1[5])
+    print(L1[99])
+    if 77 not in L1:
+        print("Key Missing")
 
+    # By default is you use a list
+    # Then 0 -> are
+    # but 1 -> you instead of our desired 5
+    # and 2 -> home
+    L2 = ["are", "you", "home"]
+
+    # But if you still want to map 99 -> home
+    # then you need to create a list with 100 items
+    # Lists for sequential indices
+    # Lists are akin to arrays in other languages
 
     # Dict as a matrix
     X = 3 # Dimensions
@@ -14,7 +29,8 @@ if __name__ == '__main__':
     for i in range(X):
         for j in range(Y):
             for k in range(Z):
-                M[i, i, k] = i * j * k
+                # Uses tuple of i, j, k as key
+                M[i, j, k] = i * j * k
     print(M)
 
     # Create dict from two lists using the zip function
